3afa9e1122b920d8b458cc864464b2f82325552b
mahesh-143 <maheshodedara13@yahoo.com>
2023-06-18 21:35:03 +0530

    Adjusted font weight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="font-medium">
       <body className={poppins.className}>
          <Providers>{children}</Providers>
         </body>
