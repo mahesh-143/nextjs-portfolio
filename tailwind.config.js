1a8cfc379a443606f7971cf7582553522391fd58
mahesh-143 <maheshodedara13@yahoo.com>
2023-06-18 09:46:47 +0530

    theme configurations and added assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'noise-texture': "url('/noiseandtexture.png')",
+        'noise-texture-dark': "url('/noiseandtexturedark.png')"
       },
     },
   },
