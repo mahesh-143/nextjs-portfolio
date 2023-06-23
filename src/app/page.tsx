5bff3a6e6beb0b956c3c2511ecb07f2eccc99e43
mahesh-143 <maheshodedara13@yahoo.com>
2023-06-23 19:23:03 +0530

    moved bg and font style to globals.css from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image"
 import Navbar from "./components/sections/Navbar"
 import Hero from "./components/sections/Hero"
-import ThemeSwitch from "./components/ui/ThemeSwitch"
 import About from "./components/sections/About"
 import Projects from "./components/sections/Projects"
 import Contact from "./components/sections/Contact"
@@ -9,7 +7,7 @@ import Footer from "./components/sections/Footer"
 
 export default function Home() {
   return (
-    <div className="bg-noise-texture dark:bg-noise-texture-dark text-xl">
+    <div>
       <Navbar />
       <Hero />
       <About />
