f2ed92f71bfe94beaf95917be3890a8740bc8ea1
mahesh-143 <maheshodedara13@yahoo.com>
2023-06-24 21:33:26 +0530

    Added email address and subheadline in contact section

--- src/app/components/sections/Contact.tsx
+++ src/app/components/sections/Contact.tsx
@@ -2,6 +2,7 @@
 import React from "react"
 import { inter } from "../utils/fonts"
 import { motion } from "framer-motion"
+import { FaPaperPlane } from "react-icons/fa"
 
 const variants = {
   hidden: { opacity: 0, x: 0, y: 20 },
@@ -12,10 +13,12 @@ const Contact = () => {
   return (
     <motion.div initial="hidden" animate="enter" transition={{duration: 0.4, type: "easeInOut"}} variants={variants} className="container mx-auto px-5 pt-10" id="Contact">
       <h2
-        className={`${inter.className} font-bold text-5xl md:text-6xl text-center mb-20`}
+        className={`${inter.className} font-bold text-5xl md:text-6xl text-center mb-14`}
       >
         Contact Me
       </h2>
+      <p className="mb-8 text-center max-w-3xl mx-auto">I am available for exciting UX design and frontend development opportunities. Get in touch today!</p>
+      <div className="text-center mx-auto font-bold py-2 px-2 mb-8 max-w-fit border-4 border-black dark:border-whiteaccent rounded-xl flex items-center gap-2"><FaPaperPlane/><span>maheshodedara13&#64;yahoo.com</span> </div>
       <div>
         <form action="" className="flex flex-col gap-1 max-w-3xl mx-auto">
             <label htmlFor="name">name</label>
